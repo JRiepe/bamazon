7649380a1ea9b5ec37490127f7ccb78edc505968
John D. Riepe <JohnD.Riepe@Danielles-MacBook-Pro.local>
2016-06-21 16:03:45 -0400

    initial commit

---
+++ BamazonCustomer.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: process.argv[2],
+	database: 'Bamazon'
+})
+
+connection.connect(function(err) {
+	if (err) throw err;
+	console.log('connected as id'+ connection.threadId)
+});
+
+connection.query('SELECT * FROM Products', function(err, data) {
+	if (err) throw err;
+	console.log(data);
+ }); // first argument string, second argument a  function
